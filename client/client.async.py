ab3c12937330b51e1890003e8d23734b689bc4be
Georgy <ggeorgie@redhat.com>
2019-05-10 15:09:08 +0200

    Skeleton code for server and client ws connection

--- client/client.async.py
+++ client/client.async.py
@@ -3,28 +3,53 @@ import asyncio
 import websockets
 import os
 import json
-import requests
 import logging
+import time
+import aiohttp
+import ssl
+
+# json build-config payload & http endpoint
+# {"kind":"BuildRequest","apiVersion":"build.openshift.io/v1","metadata":{"name":"indy-perf","creationTimestamp":None},"triggeredBy":[{"message":"Manually triggered"}],"dockerStrategyOptions":{},"sourceStrategyOptions":{}}
+# 'https://paas.upshift.redhat.com:443/apis/build.openshift.io/v1/namespaces/nos-perf/buildconfigs/indy-perf/instantiate'
+
+# http endpoint for github webhooks
+# https://paas.upshift.redhat.com/oapi/v1/namespaces/nos-perf/buildconfigs/indy-perf/webhooks/fWXhF3kdYJQsbVZdvRqS/github
 
 async def start():
-    async with websockets.connect(os.getenv('WS_SERVER',"ws://localhost:8182/ws")) as websocket:
-        await websocket.send(os.getenv('MSG','TEST MESSAGE'))
-        while True:
-            response = await websocket.recv()
-            print(response)
-#           http POST on https://paas.upshift.redhat.com/oapi/v1/namespaces/nos-perf/buildconfigs/indy-perf/webhooks/fWXhF3kdYJQsbVZdvRqS/github
-            token = open('/var/run/secrets/kubernetes.io/serviceaccount/token').read()
-            data = json.loads(response)
-            url = os.getenv('URL_TRIGGER','https://indy-perf:8080/oapi/v1/namespaces/nos-perf/buildconfigs/indy-perf/webhooks/fWXhF3kdYJQsbVZdvRqS/github')
-            alowed = ["Accept" , "User-Agent" , "X-Github-Event" , "X-Github-Delivery" , "Content-Type" ]
-            headers = {x:y for x,y in data["headers"].items() if x in alowed}
-            headers['Authorization'] = "Bearer {}".format(token)
-            payload = json.loads(data["payload"])
-            resp = requests.post(
-                url = url,
-                data = payload,
-                headers=headers
-            )
-            print(resp)
+    token = open('/var/run/secrets/kubernetes.io/serviceaccount/token').read()
+    cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+    build_url = os.getenv('URL_TRIGGER','https://paas.upshift.redhat.com/oapi/v1/namespaces/nos-perf/buildconfigs/indy-perf/webhooks/fWXhF3kdYJQsbVZdvRqS/github')
+    ws_endpoint = os.getenv('WS_SERVER', "ws://ws-server-nos-perf.7e14.starter-us-west-2.openshiftapps.com/ws")
+    alowed = ["Accept", "User-Agent", "X-Github-Event", "X-Github-Delivery", "Content-Type", "X-Hub-Signature"]
+
+    sslcontext = ssl.create_default_context(cafile=cert)
+    conn = aiohttp.TCPConnector(ssl_context=sslcontext)
+
+
+    async with websockets.connect(ws_endpoint) as websocket:
+        logging.warning('Opened Connection to Server')
+        await websocket.send(json.dumps({'msg':'TEST MESSAGE'}))
+        async with aiohttp.ClientSession(connector=conn) as session:
+
+            while True:
+                response = await websocket.recv()
+                logging.warning('-- Recived: {}'.format(response))
+                data = json.loads(response)
+                payload = json.loads(data["payload"])
+                # adding metadata and triggeredBy keys just for testing starting build job
+                payload["metadata"] = {
+                    "name":"indy-perf"
+                }
+                payload["triggeredBy"] = {}
+                # end of test added key values
+                headers = {x:y for x,y in data["headers"].items() if x in alowed}
+                headers['Authorization'] = "Bearer {}".format(token)
+                headers['Accept'] = "application/json"
+                if response is None: break
+                else:
+                    async with session.post(build_url, data=json.dumps(payload), headers=headers) as resp:
+                        logging.warning(resp.status)
+                        logging.warning(await resp.text())
+
 
 asyncio.get_event_loop().run_until_complete(start())
--- server/server.py
+++ server/server.py
@@ -6,34 +6,43 @@ import time
 import uuid
 import json
 import datetime
-
+import logging
+import urllib
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 
     SESSIONS = []
 
     def open(self, *args, **kwargs):
-        print('Connection is open')
+        logging.warning('Connection is open')
         if self not in self.SESSIONS:
             self.SESSIONS.append(self)
-            print('Client Session added: {}'.format(self))
+            logging.warning('Client Session added: {}'.format(self))
 
     def on_close(self):
-        print('Connection is closed')
+        logging.warning('Connection is closed')
         if self in self.SESSIONS:
             self.SESSIONS.remove(self)
-            print('Client Session removed from sessions: {}'.format(self))
+            logging.warning('Client Session removed: {}'.format(self))
 
     def on_message(self, message):
-        print('Message recived: {}'.format(message))
+        logging.warning('Message recived: {}'.format(message))
 
     @classmethod
     def broadcast(cls,data):
         for session in cls.SESSIONS:
+            logging.warning("--- Broadcast to {} ---".format(session))
             session.write_message(data)
 
+    @classmethod
+    def heartbeat(cls):
+        logging.warning("--- Hearth Bit ---")
+        return "ok"
+
     def check_origin(self, origin):
-        return True
+        logging.warning("--- Check Origin ---> {}".format(origin))
+        parsed_origin = urllib.parse.urlparse(origin)
+        return parsed_origin.netloc.endswith(".redhat.com")
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -41,15 +50,13 @@ class MainHandler(tornado.web.RequestHandler):
         self.write({'status':'ok'})
 
     def post(self, *args, **kwargs):
-        headers = {
-            x: self.request.headers[x] for x in self.request.headers
-        }
-        payload = self.request.body.decode('utf-8')
+        headers = {x: self.request.headers[x] for x in self.request.headers}
+        payload = self.request.body
         data = {
             'id': str(uuid.uuid4()),
             'headers': headers,
             'timestamp': str(int(time.time() * 1000)),
-            'payload': payload
+            'payload': payload.decode('utf-8')
         }
         WSHandler.broadcast(data)
         self.write(json.dumps({
